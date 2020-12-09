0ba7023b1117fbadd1b942c5181a3b87b569f65c
Mark Dickinson <dickinsm@gmail.com>
2020-12-09 15:53:33 +0000

    Bump version in setup.py (#6)
    Bump version to 1.1.0. Main changes since 1.0.0:
    
    - Significant simplification of the internal logic
    - Include a proof of correctness in the docstrings
    - Additional testing

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r", encoding="utf-8") as readme_file:
 
 setuptools.setup(
     name="simplefractions",
-    version="1.0.0",
+    version="1.1.0",
     author="Mark Dickinson",
     author_email="dickinsm@gmail.com",
     description="Find the simplest fraction for a given float or interval",
