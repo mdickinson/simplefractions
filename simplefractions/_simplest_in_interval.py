ec9940cbfd1ad4b37f5becbf09e16ac3d7f14165
Mark Dickinson <dickinsm@gmail.com>
2020-12-09 14:52:32 +0000

    Yet more cleanup

--- simplefractions/_simplest_in_interval.py
+++ simplefractions/_simplest_in_interval.py
@@ -6,34 +6,6 @@ Proof of correctness
 
 Here's a sketch of a proof of correctness for _simplest_in_interval_pos.
 
-We assume that on input to _simplest_in_interval_pos:
-
-- right_numerator > 0, or right_numerator == 0 and right_closed is True
-- the interval described is nonempty, so left < right (or left == right
-  with both endpoints closed)
-- the interval doesn't include either -inf or inf
-
-First we deal with a couple of special cases:
-
-1. If left_numerator < 0, then (given our assumptions on the
-   right endpoint), the interval contains 0, and 0 is the simplest
-   fraction in the interval. But in this case we have
-   left_numerator + left_denominator < left_denominator,
-   so on entry to the while statement, t > s >= s - left_closed.
-   So the while block is not executed, and we immediately return
-   the pair (c + a, d + b), which has value (0, 1), the correct result.
-
-2. Similarly, if left_numerator == 0 and left_closed is True, then on entry to
-   the while statement, t == s, so the while block is again not executed, and
-   we immediately return with the correct result of (0, 1).
-
-If we're not in either of the above special cases, then either
-0 < left_numerator and so t < s, or 0 == left_numerator and left_closed
-is False. In both cases, the condition at the top of the while loop
-is satisfied, so we end up executing the while loop at least once.
-
-From this point on we assume that we're not in either case 1 or case 2
-above, so our interval is a subinterval of the positive real line.
 
 Loop invariants
 ---------------
@@ -41,38 +13,35 @@ Loop invariants
 The following invariants hold at the beginning and end of every while loop
 iteration::
 
-    s * v < u * t or (s * v == u * t and st_closed and uv_closed)
-    0 < s or (0 == s and not st_closed)
+    0 <= a, 0 <= b, 0 <= c, 0 <= d
+    t <= r, 0 < s, 0 < u, w <= v
+    r * v < u * s + t * w
 
-Additionally, on every *even* iteration (assuming that we start with
-iteration 0, so the first iteration is even):
+(for example, in the case of an open interval we have 0 <= r/s < u/v <= inf at
+all times, while for a closed interval we have 0 < r/s <= u/v < inf at all
+times).
 
-    a * s + b * t == left_numerator
-    c * s + d * t == left_denominator
-    st_closed == left_closed
-    a * u + b * v == right_numerator
-    c * u + d * v == right_denominator
-    uv_closed == right_closed
+We can always recover the original values of r, s, t, u, v and w from the
+current values. Writing r0 through w0 for the original values, on every *even*
+iteration (considering the first iteration to be even) we have:
+
+    (a * r + b * s, c * r + d * s, t) == (r0, s0, t0)
+    (a * u + b * v, c * u + d * v, w) == (u0, v0, w0)
     a * d - b * c == 1
 
 while on every *odd* iteration:
 
-    a * s + b * t == right_numerator
-    c * s + d * t == right_denominator
-    st_closed == right_closed
-    a * u + b * v == left_numerator
-    c * u + d * v == left_denominator
-    uv_closed == left_closed
+    (a * u + b * v, c * u + d * v, w) == (r0, s0, t0)
+    (a * r + b * s, c * r + d * s, t) == (u0, v0, w0)
     a * d - b * c == -1
 
 Reinterpreting this:
 
 - write I for the subinterval of the positive real line represented by the
-  inputs to _simplest_in_interval_pos. Note that each endpoint of the interval
-  may be either open or closed.
+  original inputs to _simplest_in_interval_pos.
 - at each step, write J for the subinterval of the positive real line
-  represented by the fractions s/t and u/v, together with the closure
-  information st_closed and uv_closed.
+  with endpoints r / s and u / v, closed on the left if t is True, and
+  on the right if w is True.
 - at each step, write T for the linear fractional transformation
   defined by T(z) = (az + b) / (cz + d).
 
@@ -82,19 +51,30 @@ Then at the beginning and end of each iteration of the while loop:
 - T(J) = I, and T establishes a bijection between the rationals in J
   and the rationals in I
 
-Additionally, when the while loop exits, the while loop condition
-ensures that the interval J contains the value 1. As a consequence,
-the image of that value, (a + b) / (c + d), is in I.
 
-And in fact, (a + b) / (c + d) must be the simplest fraction in I: if x/y is
-*any* fraction in the interval I (written in lowest terms), and p/q is its
-preimage (under T) in J, again written in lowest terms, then by definition of
-T:
+Termination
+-----------
 
-    x/y = (ap + bq) / (cp + dq)
+On every iteration except possibly the first, q is positive. Since the sum r +
+s + u + v decreases by q*(v+s) at each step, and s is positive, it follows that
+the sum r + s + u + v strictly decreases on every iteration except possibly the
+first. Since all of r, s, u and v are nonnegative, the loop must eventually
+terminate.
 
-moreover, since gcd(p, q) = 1 and |ad - bc| = 1, it follows that gcd(ap + bq,
-cp + dq) = 1, so both sides above are in lowest terms and we must have
+When the loop terminates, the exit condition r - t < s implies that the
+interval J contains the value 1. As a consequence, the image under T of that
+value, (a + b) / (c + d), is in I.
+
+Furthermore, (a + b) / (c + d) must be the simplest fraction in I: if x / y is
+*any* fraction in the interval I (written in lowest terms), we'll show that (a
++ b) / (c + d) is either simpler than or equal to x / y. Let p / q be the
+preimage of x / y under T; p / q is in J, so both p and q are positive. Then by
+definition of T,
+
+    x / y = (ap + bq) / (cp + dq)
+
+Since gcd(p, q) = 1 and |ad - bc| = 1, it follows that gcd(ap + bq, cp + dq) =
+1, so both sides above are in lowest terms and we must have
 
    x = ap + bq
    y = cp + dq
@@ -105,9 +85,9 @@ that
     x >= a + b
     y >= c + d
 
-So (a+b)/(c+d) is either equal to, or simpler than, x/y. Since this is true
-for all fractions x/y in I, we've successfully found the simplest fraction
-in the interval.
+So (a + b) / (c + d) is either equal to, or simpler than, x / y. Since this is
+true for all fractions x / y in I, we've successfully found the simplest
+fraction in the interval.
 """
 
 import fractions
@@ -115,53 +95,41 @@ import typing
 
 
 def _simplest_in_interval_pos(
-    left_numerator: int,
-    left_denominator: int,
-    left_closed: bool,
-    right_numerator: int,
-    right_denominator: int,
-    right_closed: bool,
+    r: int, s: int, t: bool, u: int, v: int, w: bool
 ) -> typing.Tuple[int, int]:
     """
-    Simplest fraction between two other fractions in the real line.
+    Simplest fraction in a subinterval of the positive reals.
 
-    This is the core algorithm. Given a nonempty, possibly infinite,
-    subinterval of the real line whose endpoints are rational numbers,
-    it finds the unique simplest rational number in that subinterval.
-
-    The algorithm is not quite fully general: it requires the right endpoint
-    of the interval to be positive (or zero, with right_closed being True).
+    Given a nonempty, possibly infinite, subinterval of the positive real line
+    whose endpoints are rational numbers, find the unique simplest rational
+    number in that subinterval.
 
     Parameters
     ----------
-    left_numerator, left_denominator : int
-        Numerator and denominator of the left endpoint of the interval.
-        Can be -1 and 0 (respectively) to represent -infinity.
-    left_closed : bool
+    r, s : int
+        r / s is the left endpoint of the interval.
+    t : bool
         True if left endpoint is included in the interval, else False.
-        Must be False if the left endpoint is -infinity.
-    right_numerator, right_denominator : int
-        Numerator and denominator of the right endpoint of the interval.
-        Can be 1 and 0 (respectively) to represent infinity.
-    right_closed : bool
+        Must be False if the left endpoint is 0.
+    u, v : int
+        u / v is the right endpoint of the interval. u and v can be 1
+        and 0 (respectively) to represent an infinite endpoint.
+    w : bool
         True if the right endpoint is included in the interval, else False.
         Must be False if the right endpoint is infinity.
 
     Returns
     -------
-    n, d : int
+    x, y : int
         Numerator and denominator of the simplest fraction in the interval.
     """
-    s, t = left_numerator + left_denominator, left_denominator
-    u, v = right_numerator + right_denominator, right_denominator
-    st_closed, uv_closed = left_closed, right_closed
-    a, b, c, d = 1, -1, 0, 1
-    while t <= s - st_closed:
-        q = (s - st_closed) // t
-        s, t, u, v = v, u - q * v, t, s - q * t
+    a, b, c, d = 1, 0, 0, 1
+    while True:
+        q = (r - t) // s
+        r, s, t, u, v, w = v, u - q * v, w, s, r - q * s, t
         a, b, c, d = b + q * a, a, d + q * c, c
-        st_closed, uv_closed = uv_closed, st_closed
-    return a + b, c + d
+        if r - t < s:
+            return a + b, c + d
 
 
 def _simplest_in_interval(
@@ -206,30 +174,26 @@ def _simplest_in_interval(
     if right is None and include_right:
         raise ValueError("interval may not contain infinity")
 
+    # Convert inputs to the form expected by _simplest_in_interval_pos.
+    if left is None:
+        r, s, t = (-1, 0, False)
+    else:
+        r, s, t = (left.numerator, left.denominator, include_left)
+    if right is None:
+        u, v, w = (1, 0, False)
+    else:
+        u, v, w = (right.numerator, right.denominator, include_right)
+
     # Raise on an empty interval.
-    nonempty_interval = (
-        left is None
-        or right is None
-        or left < right
-        or (left == right and include_left and include_right)
-    )
-    if not nonempty_interval:
+    if s and v and u * s + t * w <= r * v:
         raise ValueError("empty interval")
 
-    # The core routine works provided that the right endpoint is positive.
-    # If not, we negate the interval.
-    negate = right is not None and (right < 0 or (right == 0 and not include_right))
-    if negate:
-        assert right is not None  # help mypy out
-        left, right = -right, None if left is None else -left
-        include_left, include_right = include_right, include_left
-
-    n, d = _simplest_in_interval_pos(
-        -1 if left is None else left.numerator,
-        0 if left is None else left.denominator,
-        include_left,
-        1 if right is None else right.numerator,
-        0 if right is None else right.denominator,
-        include_right,
-    )
-    return fractions.Fraction(-n if negate else n, d)
+    if u + w <= 0:
+        # Subinterval of negative real line.
+        return -fractions.Fraction(*_simplest_in_interval_pos(-u, v, w, -r, s, t))
+    elif r - t < 0:
+        # Interval contains zero.
+        return fractions.Fraction(0, 1)
+    else:
+        # Subinterval of positive real line.
+        return fractions.Fraction(*_simplest_in_interval_pos(r, s, t, u, v, w))
