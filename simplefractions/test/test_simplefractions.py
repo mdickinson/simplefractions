39024566a7db5c645316dd3f670196507186ce7a
Mark Dickinson <dickinsm@gmail.com>
2022-02-20 11:29:09 +0000

    Update years in copyright headers (#17)

--- simplefractions/test/test_simplefractions.py
+++ simplefractions/test/test_simplefractions.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Mark Dickinson. All rights reserved.
+# Copyright 2020-2022 Mark Dickinson. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
